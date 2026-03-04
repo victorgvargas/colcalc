51cad445ab25c65b3f49394f3c300f5b79d732b5
Victor Vargas <vvargas2013@gmail.com>
2026-03-04 21:57:56 +0000

    feat: add Calculator component route to the main application

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './components/Home/index.tsx';
 import Layout from './components/Layout/index.tsx';
+import Calculator from './components/Calculator/index.tsx';
 
 const router = createBrowserRouter([
   {
     Component: Layout,
     children: [
       { index: true, Component: Home },
+      { path: 'calculator', Component: Calculator },
     ],
   },
 ]);
